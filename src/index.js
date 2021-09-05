6f6bdea0c7b0b85693d513031b23e804836ccabf
Shen Sabado <stephanieshen.sabado@gmail.com>
2021-09-05 13:35:24 +0800

    setup scss

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import store from './store';
+import './assets/scss/main.scss';
 
 ReactDOM.render(
   <React.StrictMode>
